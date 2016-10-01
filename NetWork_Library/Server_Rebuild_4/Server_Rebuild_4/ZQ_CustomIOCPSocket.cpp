2ddc8f8eba041f33f30c465d12daa358303ed9e6
mikesimb <mikesimb@gmail.com>
2016-10-01 15:54:54 +0800

    完善了定义。

--- NetWork_Library/Server_Rebuild_4/Server_Rebuild_4/ZQ_CustomIOCPSocket.cpp
+++ NetWork_Library/Server_Rebuild_4/Server_Rebuild_4/ZQ_CustomIOCPSocket.cpp
@@ -30,7 +30,50 @@ void CZQ_CustomIOCPSocket::setServerIP(CZQString IPAddress)
 {
 	if (_IOCPSOCKET.socket == INVALID_SOCKET && IPAddress.GetLength() <= 0)
 	{
+		memset(_IOCPSOCKET.IP, 0, 16);
+		memcpy(_IOCPSOCKET.IP, (char*)IPAddress, IPAddress.GetLength());
 
+	}
+}
+
+int CZQ_CustomIOCPSocket::getServerPort()
+{
+	return _IOCPSOCKET.port;
+}
+
+void CZQ_CustomIOCPSocket::setServerPort(const int Port)
+{
+	if(_IOCPSOCKET.socket == INVALID_SOCKET)
+		 _IOCPSOCKET.port = Port;
+
+}
+
+bool CZQ_CustomIOCPSocket::isAcriveed()
+{
+	return _IOCPSOCKET.socket != INVALID_SOCKET;
+
+}
+
+void CZQ_CustomIOCPSocket::open()
+{
+	setActive(true);
+}
+
+void CZQ_CustomIOCPSocket::close()
+{
+	setActive(false);
+}
+
+void CZQ_CustomIOCPSocket::socketErrorEvent(CZQ_CustomIOCPClientSocket* clientsocket, int errorCode)
+{
+	if (OnSocketError != nullptr)
+	{
+		OnSocketError(clientsocket, errorCode);
+	}
+	if (errorCode != 0)
+	{
+		//OutputDebugString("SocketError Code = %d",ErrorCode);
+		//这里可以打印错误信息
 	}
 }
 
